237c5bcd4f407ca7ff442b8720e3592056bc9d85
NehaChopra <nchopra456@gmail.com>
2020-03-01 13:50:37 +0530

    kafka utils

--- kafka-events-util/src/main/java/com/quintifi/kafkaeventsutil/controller/TaskController.java
+++ kafka-events-util/src/main/java/com/quintifi/kafkaeventsutil/controller/TaskController.java
@@ -1,10 +1,14 @@
 package com.quintifi.kafkaeventsutil.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.quintifi.kafkaeventsutil.domain.network.request.BaseRequest;
 import com.quintifi.kafkaeventsutil.service.KafkaAsyncProducerService;
 import com.quintifi.kafkaeventsutil.service.KafkaProducerService;
 
@@ -24,10 +28,97 @@ public class TaskController {
 	@Autowired
 	private KafkaAsyncProducerService kafkaAsyncProducerService;
 
-	// @GetMapping("/api/todoTasks")
-	// public List<TodoTask> findAll() {
-	// LOGGER.info("Inside function findAll of TodoTaskController............
-	// Retrieving tasks !!");
-	// return todoTaskService.findAll();
-	// }
+	@PostMapping("/sync/payload")
+	public void pushPayload(BaseRequest request) {
+		LOGGER.debug("Inside function findAll of TodoTaskController...........Retrieving tasks !!");
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && !ObjectUtils.isEmpty(request.getPartition())
+				&& StringUtils.isNotEmpty(request.getKey()) && !ObjectUtils.isEmpty(request.getTimestamp())
+				&& StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaProducerService.sendMessage(request.getTopic(), request.getPartition(), request.getKey(),
+						request.getTimestamp(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && !ObjectUtils.isEmpty(request.getPartition())
+				&& StringUtils.isNotEmpty(request.getKey()) && StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaProducerService.sendMessage(request.getTopic(), request.getPartition(), request.getKey(),
+						request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && StringUtils.isNotEmpty(request.getKey())
+				&& StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaProducerService.sendMessage(request.getTopic(), request.getKey(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaProducerService.sendMessage(request.getTopic(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@PostMapping("/async/payload")
+	public void pushAsyncPayload(BaseRequest request) {
+		LOGGER.debug("Inside function findAll of TodoTaskController...........Retrieving tasks !!");
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && !ObjectUtils.isEmpty(request.getPartition())
+				&& StringUtils.isNotEmpty(request.getKey()) && !ObjectUtils.isEmpty(request.getTimestamp())
+				&& StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaAsyncProducerService.sendMessage(request.getTopic(), request.getPartition(), request.getKey(),
+						request.getTimestamp(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && !ObjectUtils.isEmpty(request.getPartition())
+				&& StringUtils.isNotEmpty(request.getKey()) && StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaAsyncProducerService.sendMessage(request.getTopic(), request.getPartition(), request.getKey(),
+						request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && StringUtils.isNotEmpty(request.getKey())
+				&& StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaAsyncProducerService.sendMessage(request.getTopic(), request.getKey(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (StringUtils.isNotEmpty(request.getTopic()) && StringUtils.isNotEmpty(request.getMessage())) {
+			try {
+				kafkaAsyncProducerService.sendMessage(request.getTopic(), request.getMessage());
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
--- kafka-events-util/src/main/java/com/quintifi/kafkaeventsutil/domain/network/request/BaseRequest.java
+++ kafka-events-util/src/main/java/com/quintifi/kafkaeventsutil/domain/network/request/BaseRequest.java
@@ -16,14 +16,11 @@ public class BaseRequest implements Serializable {
 	private static final long serialVersionUID = -1898217599935891882L;
 	private String payload;
 	private String info;
-
-	public String getInfo() {
-		return info;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
+	private String topic;
+	private String key;
+	private String message;
+	private Integer partition;
+	private Long timestamp;
 
 	public String getPayload() {
 		return payload;
@@ -33,9 +30,58 @@ public class BaseRequest implements Serializable {
 		this.payload = payload;
 	}
 
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Integer getPartition() {
+		return partition;
+	}
+
+	public void setPartition(Integer partition) {
+		this.partition = partition;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
 	@Override
 	public String toString() {
-		return "BaseRequest [payload=" + payload + ", info=" + info + "]";
+		return "BaseRequest [payload=" + payload + ", info=" + info + ", topic=" + topic + ", key=" + key + ", message="
+				+ message + ", partition=" + partition + ", timestamp=" + timestamp + "]";
 	}
 
 }
